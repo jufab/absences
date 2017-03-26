e52bd3e28bfadab5fd4d7c8c7ba8852a0cac5770
jufab <julien.fabre.33@gmail.com>
2017-03-26 23:48:55 +0200

    ajout de test

--- src/test/java/fr/jufab/springboot/repository/PersonneRepositoryTest.java
+++ src/test/java/fr/jufab/springboot/repository/PersonneRepositoryTest.java
@@ -2,11 +2,15 @@ package fr.jufab.springboot.repository;
 
 import fr.jufab.springboot.domain.Personne;
 import fr.jufab.springboot.domain.repository.PersonneRepository;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -18,11 +22,25 @@ public class PersonneRepositoryTest {
     private PersonneRepository repository;
 
     @Test
-    public void testPersonne() throws Exception {
-        Personne personne = new Personne("FABRE","Julien");
+    public void ajouterUnePersonne() throws Exception {
+        Personne personne = new Personne("FABRE2","Julien");
         Personne newPersonne = this.repository.saveAndFlush(personne);
-        assertThat(newPersonne.getNom()).isEqualTo("FABRE");
+        assertThat(newPersonne.getNom()).isEqualTo("FABRE2");
         assertThat(newPersonne.getPrenom()).isEqualTo("Julien");
+    }
+
+    @Test
+    @Sql("personnes.sql")
+    public void verifierUneListeDePersonnes() throws Exception {
+        List<Personne> personnes = this.repository.findAll();
+        assertThat(personnes.size()).isEqualTo(3);
+    }
+
+    @Test
+    public void supprimerUnePersonne() throws Exception {
+        Personne personne = new Personne("ASUPPRIMER","Julien");
+        Personne newPersonne = this.repository.saveAndFlush(personne);
+        this.repository.delete(newPersonne);
     }
 
 }
---
+++ src/test/resources/fr/jufab/springboot/repository/personnes.sql
@@ -0,0 +1,3 @@
+INSERT INTO personne(nom, prenom) VALUES ('FABRE', 'Julien');
+INSERT INTO personne(nom, prenom) VALUES ('DURAND', 'Julien');
+INSERT INTO personne(nom, prenom) VALUES ('DUPONT', 'Julien');
