46b57dbd52279428e50f22de6de8228ef355a79a
jufab <julien.fabre.33@gmail.com>
2017-04-02 00:08:31 +0200

    ajout Heroku + conf database

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
+[![Build Status](https://travis-ci.org/jufab/absences.svg?branch=master)](https://travis-ci.org/jufab/absences)
+
+
 # absences
 Test Spring Boot
+
+# Deploy to:
+[![Deploy to Heroku](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
--- src/test/java/fr/jufab/springboot/service/PersonneServiceTest.java
+++ src/test/java/fr/jufab/springboot/service/PersonneServiceTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -14,6 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 @Sql("classpath:fr/jufab/springboot/repository/personnes.sql")
 public class PersonneServiceTest {
 
