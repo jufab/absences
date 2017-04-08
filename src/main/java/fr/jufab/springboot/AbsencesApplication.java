abad39693383039bb455ac644d8ef7b3cc315fab
jufab <julien.fabre.33@gmail.com>
2017-04-08 23:46:40 +0200

    ooops...

--- src/main/java/fr/jufab/springboot/AbsencesApplication.java
+++ src/main/java/fr/jufab/springboot/AbsencesApplication.java
@@ -1,10 +1,11 @@
 package fr.jufab.springboot;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AbsencesApplication {
-	/*public static void main(String[] args) {
+	public static void main(String[] args) {
 		SpringApplication.run(AbsencesApplication.class, args);
-	}*/
+	}
 }
