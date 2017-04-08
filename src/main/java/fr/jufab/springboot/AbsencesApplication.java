aa556957190c548f4ed6eefe1bff9b3b04f53195
jufab <julien.fabre.33@gmail.com>
2017-04-09 01:36:17 +0200

    modification datasource et ajout profile dev

--- src/main/java/fr/jufab/springboot/AbsencesApplication.java
+++ src/main/java/fr/jufab/springboot/AbsencesApplication.java
@@ -2,8 +2,11 @@ package fr.jufab.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(DataSourceProperties.class)
 public class AbsencesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AbsencesApplication.class, args);
